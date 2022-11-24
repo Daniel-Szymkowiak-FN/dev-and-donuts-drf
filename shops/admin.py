ae20866c25f333f98ed38ea082b110aed239b6b6
DanielSzym <daniel.szymkowiak@futurenext.de>
2022-11-24 16:20:14 +0100

    Add commented out Donut model to admin

--- shops/admin.py
+++ shops/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Shop
+from .models import Shop  # , Donut
 
 # Register your models here.
 class DisplayShop(admin.ModelAdmin):
@@ -13,3 +13,22 @@ class DisplayShop(admin.ModelAdmin):
 
 
 admin.site.register(Shop, DisplayShop)
+
+
+# class DisplayDonut(admin.ModelAdmin):
+#     """
+#     Configures the display of the User Admin Panel.
+#     """
+
+#     list_display = ("pk", "shop", "name", "user_email")
+#     search_fields = ("pk", "shop__user__email", "name")
+#     readonly_fields = ("pk", "last_updated")
+
+#     def user_email(self, obj):
+#         try:
+#             return obj.shop.user.email
+#         except:
+#             return "-"
+
+
+# admin.site.register(Donut, DisplayDonut)
